5fd8f31afc3e467162c0c96ec8f31d7dc485fd0c
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-08-23 08:53:46 +0300

    (lesson_02): add zero  ase

--- lesson_01/int_example.exs
+++ lesson_01/int_example.exs
@@ -2,6 +2,9 @@ defmodule IntExample do
   def gcd(a, b) when a < 0 or b < 0 do
     gcd(abs(a), abs(b))
   end
+
+  def gcd(a, 0), do: a
+
   def gcd(a, b) do
     case rem(a, b) do
       0 -> b
@@ -13,3 +16,5 @@ end
 IntExample.gcd(5, 55) |> IO.puts
 IntExample.gcd(-5, 55) |> IO.puts
 IntExample.gcd(5, -55) |> IO.puts
+IntExample.gcd(0, 5) |> IO.puts
+IntExample.gcd(5, 0) |> IO.puts
