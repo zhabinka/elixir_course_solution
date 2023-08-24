9f727fb8ab9719d9372842e486330b0bbe24ec1c
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-08-24 18:39:33 +0300

    (lesson_04): add game solution

--- lesson_04/homework/game.exs
+++ lesson_04/homework/game.exs
@@ -1,19 +1,39 @@
 defmodule Game do
 
   def join_game(user) do
-    # TODO add your implementation
+    {:user, _, age, role} = user
+    cond do
+      role in [:admin, :moderator] -> :ok 
+      age >= 18 -> :ok
+      true -> :error
+    end
   end
 
   def move_allowed?(current_color, figure) do
-    # TODO add your implementation
+    # case figure do
+    #   {:pawn, ^current_color} -> true
+    #   {:rock, ^current_color} -> true
+    #   {_, _} -> false
+    # end
+
+    {type, color} = figure
+    type in [:pawn, :rock] and color == current_color
   end
 
   def single_win?(a_win, b_win) do
-    # TODO add your implementation
+    xor(a_win, b_win) 
   end
 
+  defp xor(x, x), do: false
+  defp xor(_x, _y), do: true
+
   def double_win?(a_win, b_win, c_win) do
-    # TODO add your implementation
+    case {a_win, b_win, c_win} do
+      {true, true, false} -> :ab
+      {true, false, true} -> :ac
+      {false, true, true} -> :bc
+      {_, _, _} -> false
+    end
   end
 
 end
