6faaec0e3276452f063615ccc9ed3aa1e9016217
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-08-30 10:10:09 +0300

    (lesson_08): add exeptions homework solution

--- lesson_08/homework/custom_exceptions/lib/homework.ex
+++ lesson_08/homework/custom_exceptions/lib/homework.ex
@@ -1,31 +1,49 @@
 defmodule IndexOutOfBoundsError do
-  
-  # TODO add your implementation
+  defexception [:index, :bound]
 
-end
-
-
-defmodule Homework do
-
-  @spec get_from_list!([any()], integer()) :: any()
-  def get_from_list!(list, index) do
-    # TODO add your implementation
+  @impl true
+  def exception({index, bound}) do
+    %IndexOutOfBoundsError{index: index, bound: bound}
   end
 
-  @spec get_from_list([any()], integer()) :: {:ok, any()} | {:error, String.t}
+  @impl true
+  def message(error) do
+    "index #{error.index} is out of bounds [0-#{error.bound})"
+  end
+end
+
+defmodule Homework do
+  @spec get_from_list!([any()], integer()) :: any()
+  def get_from_list!(list, index) do
+    case Enum.at(list, index) do
+      _ when index < 0 -> raise IndexOutOfBoundsError, {index, length(list)}
+      nil -> raise IndexOutOfBoundsError, {index, length(list)}
+      element -> element
+    end
+  end
+
+  @spec get_from_list([any()], integer()) :: {:ok, any()} | {:error, String.t()}
   def get_from_list(list, index) do
-    # TODO add your implementation
+    try do
+      element = get_from_list!(list, index)
+      {:ok, element}
+    rescue
+      error in IndexOutOfBoundsError -> {:error, Exception.message(error)}
+    end
   end
 
   @spec get_many_from_list!([any()], [integer()]) :: [any()]
   def get_many_from_list!(list, indices) do
-    # TODO add your implementation
+    Enum.map(indices, fn index -> get_from_list!(list, index) end)
   end
 
-  @spec get_many_from_list!([any()], [integer()]) :: {:ok, [any()]} | {:error, String.t}
+  @spec get_many_from_list!([any()], [integer()]) :: {:ok, [any()]} | {:error, String.t()}
   def get_many_from_list(list, indices) do
-    # TODO add your implementation
+    try do
+      resulting_list = get_many_from_list!(list, indices)
+      {:ok, resulting_list}
+    rescue
+      error in IndexOutOfBoundsError -> {:error, Exception.message(error)}
+    end
   end
-
 end
-
--- lesson_08/homework/custom_exceptions/test/homework_test.exs
+++ lesson_08/homework/custom_exceptions/test/homework_test.exs
@@ -9,18 +9,15 @@ defmodule CustomExceptionsTest do
     assert get_from_list!(list, 2) == 3
     assert get_from_list!(list, 3) == 4
 
-    assert_raise IndexOutOfBoundsError, "index 4 is out of bounds [0-4)",
-    fn ->
+    assert_raise IndexOutOfBoundsError, "index 4 is out of bounds [0-4)", fn ->
       get_from_list!(list, 4)
     end
 
-    assert_raise IndexOutOfBoundsError, "index -1 is out of bounds [0-4)",
-    fn ->
+    assert_raise IndexOutOfBoundsError, "index -1 is out of bounds [0-4)", fn ->
       get_from_list!(list, -1)
     end
 
-    assert_raise IndexOutOfBoundsError, "index 42 is out of bounds [0-4)",
-    fn ->
+    assert_raise IndexOutOfBoundsError, "index 42 is out of bounds [0-4)", fn ->
       get_from_list!(list, 42)
     end
   end
@@ -43,7 +40,7 @@ defmodule CustomExceptionsTest do
     list = ["cat", "dog", "fish"]
     assert get_many_from_list!(list, [0, 0, 2, 2]) == ["cat", "cat", "fish", "fish"]
   end
-  
+
   test "get_many_from_list!" do
     list = [10, 20, 30, 40, 50]
     assert get_many_from_list!(list, [0, 1]) == [10, 20]
@@ -52,13 +49,11 @@ defmodule CustomExceptionsTest do
     assert get_many_from_list!(list, [0, 1, 2, 3, 4]) == list
     assert get_many_from_list!(list, [1, 1, 3, 3, 3, 4, 4]) == [20, 20, 40, 40, 40, 50, 50]
 
-    assert_raise IndexOutOfBoundsError, "index 5 is out of bounds [0-5)",
-    fn ->
+    assert_raise IndexOutOfBoundsError, "index 5 is out of bounds [0-5)", fn ->
       get_many_from_list!(list, [0, 1, 5])
     end
 
-    assert_raise IndexOutOfBoundsError, "index 10 is out of bounds [0-5)",
-    fn ->
+    assert_raise IndexOutOfBoundsError, "index 10 is out of bounds [0-5)", fn ->
       get_many_from_list!(list, [0, 10, 2])
     end
   end
@@ -78,6 +73,4 @@ defmodule CustomExceptionsTest do
     assert get_many_from_list(list, [0, 3, 1]) == {:error, "index 3 is out of bounds [0-3)"}
     assert get_many_from_list(list, [-1, 2]) == {:error, "index -1 is out of bounds [0-3)"}
   end
-  
-  
 end
