b8bb88e8b74477f66550dde3d3e2994fee7efa09
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-08-30 15:22:54 +0300

    (lesson_09): add chain validation solution

--- lesson_09/homework/chat_room/lib/chat_room.ex
+++ lesson_09/homework/chat_room/lib/chat_room.ex
@@ -1,22 +1,48 @@
 defmodule ChatRoom do
-
   alias ChatRoomModel, as: M
-  
-  @spec join_room(M.user_name, M.room_name) :: :ok | {:error, atom}
+
+  @spec join_room(M.user_name(), M.room_name()) :: :ok | {:error, atom}
   def join_room(user_name, room_name) do
-    # TODO add your implementation
+    case get_user(user_name) do
+      {:ok, user} ->
+        case get_room(room_name) do
+          {:ok, room} ->
+            case reached_limit?(room) do
+              true ->
+                {:error, :room_reached_limit}
+
+              false ->
+                case public?(room) do
+                  true ->
+                    :ok
+
+                  false ->
+                    case member?(user, room) do
+                      true -> :ok
+                      false -> {:error, :not_allowed}
+                    end
+                end
+            end
+
+          {:error, :not_found} ->
+            {:error, :room_not_found}
+        end
+
+      {:error, :not_found} ->
+        {:error, :user_not_found}
+    end
   end
-  
+
   @users [
     %M.User{name: "User 1"},
     %M.User{name: "User 2"},
     %M.User{name: "User 3"}
   ]
-  
+
   @rooms [
     %M.Room{name: "Room 1", type: :public},
     %M.Room{name: "Room 2", type: :private, members: ["User 1", "User 2"]},
-    %M.Room{name: "Room 3", type: :public, limit: 10},
+    %M.Room{name: "Room 3", type: :public, limit: 10}
   ]
 
   @online %{
@@ -25,35 +51,38 @@ defmodule ChatRoom do
     "Room 3" => 10
   }
 
-  @spec get_user(M.user_name) :: {:ok, M.User.t} | {:error, :not_found}
+  @spec get_user(M.user_name()) :: {:ok, M.User.t()} | {:error, :not_found}
   def get_user(name) do
-    res = Enum.find(
-      @users,
-      fn(user) -> user.name == name end
-    )
+    res =
+      Enum.find(
+        @users,
+        fn user -> user.name == name end
+      )
+
     if res, do: {:ok, res}, else: {:error, :not_found}
   end
 
-  @spec get_room(M.room_name) :: {:ok, M.Room.t} | {:error, :not_found}
+  @spec get_room(M.room_name()) :: {:ok, M.Room.t()} | {:error, :not_found}
   def get_room(name) do
-    res = Enum.find(
-      @rooms,
-      fn(%M.Room{name: room_name}) -> room_name == name end
-    )
+    res =
+      Enum.find(
+        @rooms,
+        fn %M.Room{name: room_name} -> room_name == name end
+      )
+
     if res, do: {:ok, res}, else: {:error, :not_found}
   end
 
-  @spec public?(M.Room.t) :: boolean
+  @spec public?(M.Room.t()) :: boolean
   def public?(room), do: room.type == :public
 
-  @spec member?(M.User.t, M.Room.t) :: boolean
+  @spec member?(M.User.t(), M.Room.t()) :: boolean
   def member?(user, room) do
     Enum.member?(room.members, user.name)
   end
 
-  @spec reached_limit?(M.Room.t) :: boolean
+  @spec reached_limit?(M.Room.t()) :: boolean
   def reached_limit?(room) do
     room.limit <= @online[room.name]
   end
-
 end
--- lesson_09/homework/chat_room/lib/chat_room_model.ex
+++ lesson_09/homework/chat_room/lib/chat_room_model.ex
@@ -1,30 +1,29 @@
 defmodule ChatRoomModel do
-
-  @type user_name :: String.t
-  @type room_name :: String.t
+  @type user_name :: String.t()
+  @type room_name :: String.t()
   @type room_type :: :public | :private
 
   defmodule User do
     alias ChatRoomModel, as: M
-    
+
     @type t :: %__MODULE__{
-      name: M.user_name
-    }
+            name: M.user_name()
+          }
     @enforce_keys [:name]
     defstruct [
       :name
     ]
   end
-  
+
   defmodule Room do
     alias ChatRoomModel, as: M
 
     @type t :: %__MODULE__{
-      name: M.room_name,
-      type: M.room_type,
-      members: [M.user_name],
-      limit: pos_integer
-    }
+            name: M.room_name(),
+            type: M.room_type(),
+            members: [M.user_name()],
+            limit: pos_integer
+          }
     @enforce_keys [:name]
     defstruct [
       :name,
@@ -33,5 +32,4 @@ defmodule ChatRoomModel do
       {:limit, 100}
     ]
   end
-
 end
