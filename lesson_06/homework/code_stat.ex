02863ac895d2f007a124fbc6d618db4b514fae69
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-08-31 10:14:15 +0300

    (lesson_06): refactor stat solution

--- lesson_06/homework/code_stat.ex
+++ lesson_06/homework/code_stat.ex
@@ -19,28 +19,52 @@ defmodule CodeStat do
   @max_depth 5
 
   def analyze(path) do
-    files =
-      get_files(path)
-      |> Enum.filter(fn file ->
-        Path.extname(file) not in @ignore_extensions and
-          Path.basename(file) not in @ignore_names
-      end)
-
-    init =
+    initial =
       Enum.reduce(@types, %{}, fn {lang, _}, acc ->
         Map.put(acc, lang, %{files: 0, lines: 0, size: 0})
       end)
 
-    combiner = fn file, acc ->
-      {lang, %{files: f, lines: l, size: s} = stat} = get_file_info(file)
+    analyze(path, initial)
+  end
 
-      Map.update(acc, lang, stat, fn value ->
-        %{files: files, lines: lines, size: size} = value
-        %{files: files + f, lines: lines + l, size: size + s}
-      end)
+  def analyze(path, acc) do
+    cond do
+      Path.basename(path) in @ignore_names ->
+        %{}
+
+      Path.extname(path) in @ignore_extensions ->
+        %{}
+
+      File.regular?(path) ->
+        file_stat = get_file_info(path)
+        Map.merge(acc, file_stat, &merge_stat/3)
+
+      File.dir?(path) ->
+        File.ls!(path)
+        # |> Enum.map(fn dir -> Path.join(path, dir) end)
+        |> Enum.map(&Path.join(path, &1))
+        |> Enum.reduce(
+          acc,
+          fn dir, nested ->
+            Map.merge(nested, analyze(dir, acc), &merge_stat/3)
+          end
+        )
     end
+  end
 
-    files |> Enum.reduce(init, combiner)
+  def merge_stat(_k, v1, v2) do
+    %{files: f1, lines: l1, size: s1} = v1
+    %{files: f2, lines: l2, size: s2} = v2
+    %{files: f1 + f2, lines: l1 + l2, size: s1 + s2}
+  end
+
+  def get_file_info(path) do
+    {:ok, %{size: size}} = File.stat(path)
+    {:ok, contents} = File.read(path)
+    lines_count = contents |> String.split("\n") |> length
+    type = get_type_file(path)
+
+    %{type => %{files: 1, lines: lines_count, size: size}}
   end
 
   def get_type_file(path) do
@@ -53,25 +77,43 @@ defmodule CodeStat do
     end
   end
 
-  def get_file_info(path) do
-    {:ok, %{size: size}} = File.stat(path)
-    {:ok, contents} = File.read(path)
-    lines_count = contents |> String.split("\n") |> length
-    type = get_type_file(path)
+  # def _analyze(path) do
+  #   initial =
+  #     Enum.reduce(@types, %{}, fn {lang, _}, acc ->
+  #       Map.put(acc, lang, %{files: 0, lines: 0, size: 0})
+  #     end)
+  #
+  #   traversal(get_files(path), initial)
+  # end
 
-    {type, %{files: 1, lines: lines_count, size: size}}
-  end
-
-  def get_files(path) do
-    cond do
-      File.regular?(path) ->
-        [path]
-
-      File.dir?(path) ->
-        File.ls!(path)
-        |> Enum.map(&Path.join(path, &1))
-        |> Enum.map(&get_files/1)
-        |> List.flatten()
-    end
-  end
+  # def get_files(path) do
+  #   cond do
+  #     Path.basename(path) in @ignore_names ->
+  #       %{}
+  #
+  #     Path.extname(path) in @ignore_extensions ->
+  #       %{}
+  #
+  #     File.regular?(path) ->
+  #       get_file_info(path)
+  #
+  #     File.dir?(path) ->
+  #       File.ls!(path)
+  #       |> Enum.map(&Path.join(path, &1))
+  #       |> Enum.map(&get_files/1)
+  #   end
+  # end
+  #
+  # def traversal([], acc), do: acc
+  #
+  # def traversal([[head | children] | siblings], acc) do
+  #   new_acc = Map.merge(acc, head, &merge_lang/3)
+  #
+  #   traversal(siblings ++ children, new_acc)
+  # end
+  #
+  # def traversal([head | tail], acc) do
+  #   new_acc = Map.merge(acc, head, &merge_lang/3)
+  #   traversal(tail, new_acc)
+  # end
 end
