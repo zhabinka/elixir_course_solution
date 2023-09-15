12e486e67776850076c713d9e6d54cd591f33fdc
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-09-15 17:32:39 +0300

    implement server in active mode

--- lesson_15/cool_tcp_server.exs
+++ lesson_15/cool_tcp_server.exs
@@ -20,10 +20,14 @@ defmodule CoolTcpServer do
 
     @impl true
     def init(_) do
-      port = 3000
+      options = %{
+        port: 3000,
+        pool_size: 5
+      }
 
       child_spec = [
-        {CoolTcpServer.Listener, port}
+        {CoolTcpServer.AcceptorSup, :no_args},
+        {CoolTcpServer.Listener, options}
       ]
 
       Supervisor.init(child_spec, strategy: :rest_for_one)
@@ -33,30 +37,103 @@ defmodule CoolTcpServer do
   defmodule Listener do
     use GenServer
 
-    def start_link(port) do
-      GenServer.start_link(__MODULE__, port)
+    def start_link(options) do
+      GenServer.start_link(__MODULE__, options)
     end
 
     @impl true
-    def init(port) do
-      state = %{port: port}
-      IO.puts("Start Listener with state #{inspect(state)}")
+    def init(options) do
+      state = %{
+        port: Map.get(options, :port, 1234),
+        pool_size: Map.get(options, :pool_size, 50)
+      }
+
+      socket_options = [
+        :binary,
+        {:active, true},
+        {:reuseaddr, true}
+      ]
+
+      {:ok, listening_socket} = :gen_tcp.listen(state.port, socket_options)
+      state = Map.put(state, :listening_socket, listening_socket)
+      IO.puts("Start Listener on with state #{inspect(state)}")
+
+      1..state.pool_size
+      |> Enum.each(fn id -> CoolTcpServer.AcceptorSup.start_acceptor(id, listening_socket) end)
+
       {:ok, state}
+    end
+  end
+
+  defmodule AcceptorSup do
+    use DynamicSupervisor
+
+    @name :acceptor_sup
+
+    def start_link(_) do
+      DynamicSupervisor.start_link(__MODULE__, :no_args, name: @name)
+    end
+
+    @impl true
+    def init(_) do
+      DynamicSupervisor.init(strategy: :one_for_one)
+    end
+
+    def start_acceptor(id, listening_socket) do
+      child_spec = {CoolTcpServer.Acceptor, {id, listening_socket}}
+
+      DynamicSupervisor.start_child(@name, child_spec)
     end
   end
 
   defmodule Acceptor do
     use GenServer
 
-    def start_link(listening_socket) do
-      GenServer.start_link(__MODULE__, listening_socket)
+    def start_link(args) do
+      GenServer.start_link(__MODULE__, args)
     end
 
     @impl true
-    def init(listening_socket) do
-      state = %{listening_socket: listening_socket}
-      IO.puts("Start Acceptor with state #{inspect(state)}")
-      {:ok, state}
+    def init({id, listening_socket}) do
+      state = %{
+        id: id,
+        listening_socket: listening_socket
+      }
+
+      IO.puts("Start Acceptor #{state.id} with state #{inspect(state)}")
+      {:ok, state, {:continue, :wait_for_client}}
+    end
+
+    @impl true
+    def handle_continue(:wait_for_client, state) do
+      IO.puts("Acceptor #{state.id} is waiting client")
+      {:ok, socket} = :gen_tcp.accept(state.listening_socket)
+      IO.puts("Acceptor #{state.id} got client connection #{inspect(socket)}")
+      {:noreply, state}
+    end
+
+    @impl true
+    def handle_info({:tcp, socket, "quit" <> _}, state) do
+      :gen_tcp.close(socket)
+      IO.puts("Server closed connection")
+      {:noreply, state, {:continue, :wait_for_client}}
+    end
+
+    def handle_info({:tcp, socket, msg}, state) do
+      :gen_tcp.send(socket, "Server answer: #{inspect(msg)}\n")
+      {:noreply, state}
+    end
+
+    def handle_info({:tcp_closed, socket}, state) do
+      :gen_tcp.close(socket)
+      IO.puts("Client #{inspect(socket)} closed connection")
+      {:noreply, state, {:continue, :wait_for_client}}
+    end
+
+    # Catch all
+    def handle_info(msg, state) do
+      IO.puts("Acceptor #{state.id} got unknown message '#{inspect(msg)}'")
+      {:noreply, state}
     end
   end
 end
